33ee14c6b9fec4aaf9fe58fdf95415eacbfab01e
JaeWan CHOI <wkdudhksl@gamil.com>
2022-07-12 17:49:48 +0900

    FEAT - DetailView

--- item/urls.py
+++ item/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('detail/', views.DetailView.as_view())
 ]
--- item/views.py
+++ item/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Item as ItemModel
+from user.models import User as UserModel
 
-# Create your views here.
+
+class DetailView(APIView):
+
+    def get(self, request):
+        items = ItemModel.objects.all().values()
+        print(items)
+        
+        # user = UserModel.objects.get(id=request.user)
+        # print(user)
+        return Response(items)
