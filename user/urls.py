7dd10a62069ca93414982b9bb63f40dba9ca49fc
KimmyJay <fkim0801@gmail.com>
2022-07-12 17:54:25 +0900

    ADD - Signup / JWT login

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-# from .views import UserView
-# from rest_framework_simplejwt.views import TokenRefreshView # JWT refresh 토큰 발급 view 인증토큰을 재발급받기위한 또 다른 토큰 
-# from user.views import FarmTokenObtainPairView
-
+from rest_framework_simplejwt.views import TokenRefreshView # JWT refresh 토큰 발급 view 인증토큰을 재발급받기위한 또 다른 토큰 
+from user.views import (
+    TokenRefreshView,
+    FarmTokenObtainPairView,
+    UserView
+)
 
 urlpatterns = [
-    # path('', UserView.as_view()),
-#     path('api/farm/token/', FarmTokenObtainPairView.as_view(), name='farm_token'),
-#     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', UserView.as_view()),
+    path('api/farm/token/', FarmTokenObtainPairView.as_view(), name='farm_token'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
