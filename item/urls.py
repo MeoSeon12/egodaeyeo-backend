3ce600c8ea6e57f5bdb759bbd44679db239af74d
Roshu2 <kyumin1020@gmail.com>
2022-07-14 11:19:10 +0900

    FIX - kakao token to simple-jwt

--- item/urls.py
+++ item/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ItemView, DetailView
+from .views import ItemListView, DetailView
 
 
 urlpatterns = [
-    path('', ItemView.as_view(),),
+    path('', ItemListView.as_view(),),
     path('details/<int:item_id>', DetailView.as_view()),
 ]
