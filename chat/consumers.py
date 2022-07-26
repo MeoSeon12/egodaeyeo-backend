6ff7d887720ee2e63a510befed2f6a9292138ad8
Roshu2 <kyumin1020@gmail.com>
2022-07-27 00:20:29 +0900

    ADD - same as last commit

--- chat/consumers.py
+++ chat/consumers.py
@@ -11,43 +11,6 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 locale.setlocale(locale.LC_TIME, 'ko_KR')
 
-# class ChatConsumer(WebsocketConsumer):
-#     def connect(self):
-
-#         self.room_group_name = 'test'
-#         async_to_sync(self.channel_layer.group_add)(
-#             self.room_group_name,
-#             self.channel_name
-#         )
-        
-#         self.accept()
-        
-        
-#     def receive(self, text_data):
-
-#         text_data_json = json.loads(text_data)
-#         message = text_data_json['message']
-        
-#         async_to_sync(self.channel_layer.group_send)(
-#             self.room_group_name,
-#             {
-#                 'type': 'chat_message',
-#                 'message': message
-#             }
-#         )
-        
-        
-#     def chat_message(self, event):
-
-#         message = event['message']
-#         now_time = datetime.now().strftime('%p %I:%M')
-        
-#         self.send(text_data = json.dumps({
-#             'type': 'chat',
-#             'message': message,
-#             'time': now_time
-#         }))
-
 class ChatConsumer(AsyncConsumer):
     
     async def websocket_connect(self, event):
