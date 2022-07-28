e5668b22469e276a91560dfe65c754d13e76d177
JaeWan CHOI <wkdudhksl@gamil.com>
2022-07-28 17:25:08 +0900

    FEAT - Adding a Web Socket to Rental application

--- chat/consumers.py
+++ chat/consumers.py
@@ -119,3 +119,113 @@ class ChatConsumer(AsyncConsumer):
     @database_sync_to_async
     def create_chat_message(self, room, sender, content):
         ChatMessage.objects.create(room=room, user=sender, content=content)
+
+
+# 대여 신청 컨슈머
+class ContractConsumer(AsyncConsumer):
+    
+    async def websocket_connect(self, event):
+        
+        room_id = self.scope['url_route']['kwargs']['room_id']
+        chat_room = f'user_chatroom_rental_{room_id}'
+        self.chat_room = chat_room
+        
+        await self.channel_layer.group_add(
+            chat_room,
+            self.channel_name
+        )
+        await self.send({
+            'type': 'websocket.accept'
+        })
+
+    async def websocket_receive(self, event):
+        
+        received_data = json.loads(event['text'])
+        content = received_data.get('message')
+        sender_id = received_data.get('sender')
+        receiver_id = received_data.get('receiver')
+        room_id = received_data.get('room_id')
+        item_id = received_data.get('item_id')
+
+        if not content:
+            print('Error:: empty message')
+            return False
+
+        sender = await self.get_user_object(sender_id)
+        receiver = await self.get_user_object(receiver_id)
+        room_obj = await self.get_chatroom(room_id)
+        
+        if not sender:
+            print('Error:: sent by user is incorrect')
+        if not receiver:
+            print('Error:: send to user is incorrect')
+        if not room_obj:
+            print('Error:: Header id is incorrect')
+
+        await self.create_chat_message(room_obj, sender, content)
+        
+        other_user_chat_room = f'user_chatroom_{receiver_id}'
+        self_user = sender
+
+        now_date = datetime.now().strftime('%Y년 %m월 %d일 %A')
+        now_time = datetime.now().strftime('%p %I:%M')
+
+        response = {
+            'message': content,
+            'sender': self_user.id,
+            'room_id': room_id,
+            'date': now_date,
+            'time': now_time,
+            'item_id': item_id,
+        }
+        
+        #상대방 채팅창에 send
+        await self.channel_layer.group_send(
+            other_user_chat_room,
+            {
+                'type': 'chat_message',
+                'text': json.dumps(response)
+            }
+        )
+
+        #내 채팅창에 send
+        await self.channel_layer.group_send(
+            self.chat_room,
+            {
+                'type': 'chat_message',
+                'text': json.dumps(response)
+            }
+        )
+
+    async def websocket_disconnect(self, event):
+        print('disconnect', event)
+
+
+    async def chat_message(self, event):
+        await self.send({
+            'type': 'websocket.send',
+            'text': event['text'],
+        })
+    
+    
+    @database_sync_to_async
+    def get_user_object(self, user_id):
+        qs = User.objects.filter(id=user_id)
+        if qs.exists():
+            obj = qs.first()
+        else:
+            obj = None
+        return obj
+
+    @database_sync_to_async
+    def get_chatroom(self, room_id):
+        qs = ChatRoom.objects.filter(id=room_id)
+        if qs.exists():
+            obj = qs.first()
+        else:
+            obj = None
+        return obj
+
+    @database_sync_to_async
+    def create_chat_message(self, room, sender, content):
+        ChatMessage.objects.create(room=room, user=sender, content=content)
--- chat/routing.py
+++ chat/routing.py
@@ -3,4 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     path('chats/<int:user_id>', consumers.ChatConsumer.as_asgi()),
+    path('chats/contracts/<int:room_id>', consumers.ContractConsumer.as_asgi()),
 ]
