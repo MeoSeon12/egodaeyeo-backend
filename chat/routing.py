0a2fd9b01c6bd7e447cc7bd7ebaa415cdd69dad3
Roshu2 <kyumin1020@gmail.com>
2022-07-28 17:22:41 +0900

    FIX - urls , consumers.py

--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path, path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('chats/<int:user_id>', consumers.ChatConsumer.as_asgi()),
+    path('chats/<int:room_id>', consumers.ChatConsumer.as_asgi()),
 ]
--- contract/urls.py
+++ contract/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import ContractView
 
 urlpatterns = [
-    path('start/<int:item_id>', ContractView.as_view()),
-    path('end/<int:item_id>', ContractView.as_view()),
+    path('<int:item_id>', ContractView.as_view()),
 ]
