418c330ff3f88c9e9a04c5e9ac175c85ccc2e736
KimmyJay <fkim0801@gmail.com>
2022-08-02 22:04:51 +0900

    Dockerfile / docker-compose.yml / env.postgres

--- egodaeyeo/settings.py
+++ egodaeyeo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 import local_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -144,8 +145,16 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = local_settings.DATABASES
-
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('SQL_ENGINE',"django.db.backends.sqlite3"),
+        'NAME': os.environ.get('SQL_DATABASE', BASE_DIR / "db.sqlite3"),
+        'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get('SQL_PORT', '5432'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
