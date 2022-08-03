8510229eda6264e76a01b3d0b4176a6c3d1a1145
Roshu2 <kyumin1020@gmail.com>
2022-08-03 20:32:51 +0900

    FIX - asgi.py

--- egodaeyeo/asgi.py
+++ egodaeyeo/asgi.py
@@ -8,13 +8,16 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'egodaeyeo.settings')
+django.setup()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'egodaeyeo.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
