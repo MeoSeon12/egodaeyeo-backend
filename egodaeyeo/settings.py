c663409759bb7e76bd441933867a83fc986c134b
KimmyJay <fkim0801@gmail.com>
2022-08-03 14:18:01 +0900

    settings.py DATABASES edit

--- egodaeyeo/settings.py
+++ egodaeyeo/settings.py
@@ -154,6 +154,7 @@ DATABASES = {
         'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
         'HOST': os.environ.get('SQL_HOST', 'localhost'),
         'PORT': os.environ.get('SQL_PORT', '5432'),
+        'OPTIONS': os.environ.get('SQL_OPTIONS', 'utf-8'),
     }
 }
 
