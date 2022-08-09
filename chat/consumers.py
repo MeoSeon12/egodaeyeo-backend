026221ffe920205f65f8248024e9671d9a961f57
JWCHOI <wkdudhksl@naver.com>
2022-08-09 23:43:24 +0900

    FIX - Alarm functions

--- chat/consumers.py
+++ chat/consumers.py
@@ -13,9 +13,9 @@ locale.setlocale(locale.LC_TIME, 'ko_KR')
 
 # 채팅 컨슈머
 class ChatConsumer(AsyncWebsocketConsumer):
-    
+
     async def connect(self):
-        #url에 room_id를 받아서 가져온다.
+        # url에 room_id를 받아서 가져온다.
         self.room_id = self.scope['url_route']['kwargs']['room_id']
         self.room_group_name = 'chat_%s' % self.room_id
 
@@ -33,7 +33,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     async def receive(self, text_data):
-        
+
         received_data = json.loads(text_data)
         message = received_data.get('message')
         sender_id = received_data.get('sender')
@@ -48,7 +48,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         sender = await self.get_user_object(sender_id)
         receiver = await self.get_user_object(receiver_id)
         room_obj = await self.get_chatroom(room_id)
-        
+
         if not sender:
             print('Error:: sent by user is incorrect')
         if not receiver:
@@ -57,7 +57,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             print('Error:: Header id is incorrect')
 
         await self.create_chat_message(room_obj, sender, message)
-        
+
         self_user = sender
 
         now_date = datetime.now().strftime('%Y년 %m월 %d일 %A')
@@ -71,7 +71,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'time': now_time,
             'item_id': item_id,
         }
-        
+
         # 현재그룹에 send
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -125,18 +125,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
 # 거래 컨슈머
 class ContractConsumer(AsyncWebsocketConsumer):
-    
+
     async def connect(self):
-        
+
         self.room_id = self.scope['url_route']['kwargs']['room_id']
         self.room_group_name = f'contract_{self.room_id}'
-        
+
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
         await self.accept()
-
 
     async def receive(self, text_data):
         received_data = json.loads(text_data)
@@ -196,22 +195,24 @@ class ContractConsumer(AsyncWebsocketConsumer):
     def create_chat_message(self, room_id, sender_id, contract_type):
         room_obj = ChatRoom.objects.get(id=room_id)
         user_obj = User.objects.get(id=sender_id)
-        ChatMessage.objects.create(room=room_obj, user=user_obj, contract_type=contract_type, application=True)
+        ChatMessage.objects.create(
+            room=room_obj, user=user_obj, contract_type=contract_type, application=True)
         if contract_type == "수락" or contract_type == "거절":
-            contract_message = ChatMessage.objects.get(Q(room=room_obj) & ~Q(user=user_obj) & Q(contract_type="신청"))
+            contract_message = ChatMessage.objects.get(
+                Q(room=room_obj) & ~Q(user=user_obj) & Q(contract_type="신청"))
             contract_message.contract_type = "확인"
             contract_message.save()
 
 
-# 알람 컨슈머
+# 알림 컨슈머
 class AlertConsumer(AsyncConsumer):
-    
+
     async def websocket_connect(self, event):
-        
+
         user_id = self.scope['url_route']['kwargs']['user_id']
         chat_alert = f'user_chat_alert_{user_id}'
         self.chat_alert = chat_alert
-        
+
         await self.channel_layer.group_add(
             chat_alert,
             self.channel_name
@@ -222,25 +223,26 @@ class AlertConsumer(AsyncConsumer):
 
     # 웹소켓에 데이터 들어옴
     async def websocket_receive(self, event):
-        
+
         received_data = json.loads(event['text'])
         receiver_id = received_data.get('receiver')
 
         # 데이터 가공
         sender = await self.get_user_object(received_data['sender'])  # 작성자 닉네임
         title = await self.get_title_object(received_data['room_id'])
-        
+
         # 수신자에게 보낼 데이터
         response = {
             'sender': sender,
             'title': title,
             'room_id': received_data['room_id'],
             'status': received_data['status'],
+            'created_at': received_data['created_at']
         }
-        
+
         # 수신자에게 온메시지에 보냄
         other_user_chat_alert = f'user_chat_alert_{receiver_id}'
-        
+
         await self.channel_layer.group_send(
             other_user_chat_alert,
             {
@@ -252,13 +254,13 @@ class AlertConsumer(AsyncConsumer):
     # 웹소켓 연결종료
     async def websocket_disconnect(self, event):
         pass
-    
+
     async def chat_message(self, event):
         await self.send({
             'type': 'websocket.send',
             'text': event['text'],
         })
-    
+
     # 데이터 가공 (발신자 닉네임 조회)
     @database_sync_to_async
     def get_user_object(self, sender_id):
